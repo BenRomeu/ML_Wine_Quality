26218ba505c0ab5619e54da8ede6d52ce5cb24ff
donika <Donika.Arifi@etu.unige.ch>
2019-05-15 16:12:13 +0200

    Put some graphs in order to visualize our data

--- Data visualization.R
+++ Data visualization.R
@@ -1,1 +1,20 @@
+####Data Visualization####
+
+
+#### Correlation Matrix ####
+
+# Check correlation for multicollinearity 
+corrmat <- as.matrix(cor(wines[,1:12]))
+ggcorrplot(corrmat)
+
+####Boxplot of the variables####
+
+boxplot(wines_n[-12])
+
+
+#####Distribution of Good and Bad quality with 2 variables(alcohol and chlorides)####
+
+ggplot(aes(x=alcohol,y=chlorides, colour= label), data = wines_f) +
+ stat_smooth(method=loess, fullrange=TRUE, alpha = 0.1, size =1.5 )
+
 
