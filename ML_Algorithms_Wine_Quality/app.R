b6197eb4ffd86eb789bd8315164f287debbe755e
NicoKM <nicolas.moser@etu.unige.ch>
2019-05-16 10:06:22 +0200

    -removed RPART graphs -added missing library to app

--- ML_Algorithms_Wine_Quality/app.R
+++ ML_Algorithms_Wine_Quality/app.R
@@ -59,6 +59,8 @@ library(devtools)
   source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
 library(shiny)
 library(shinythemes)
+library(gridExtra)
+library(grid)
   
 
   #### Load database of red wines and white wines ####
